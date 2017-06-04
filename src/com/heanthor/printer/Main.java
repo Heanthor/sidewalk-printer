6de926f56a863a773dd8e7c45452df65b062eb1c
Reed <reedtrevelyan@gmail.com>
2017-06-03 22:47:34 -0400

    RGB to CMYK converter

--- src/com/heanthor/printer/Main.java
+++ src/com/heanthor/printer/Main.java
@@ -1,12 +1,15 @@
 package com.heanthor.printer;
 
+import com.heanthor.printer.utils.ImageUtils;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.MalformedURLException;
 
@@ -26,6 +29,9 @@ public class Main extends Application {
         String filePath = new File(getParameters().getRaw().get(0)).toURI().toURL().toString();
 
         Image img = new Image(filePath);
+        BufferedImage bufferedImage = SwingFXUtils.fromFXImage(img, null);
+        BufferedImage cmykImage = ImageUtils.rgbToCMYK(bufferedImage);
+
         ImageView iv = new ImageView(img);
         root.getChildren().add(iv);
         primaryStage.setScene(new Scene(root, 1920, 1080));
--- src/com/heanthor/printer/utils/ImageUtils.java
+++ src/com/heanthor/printer/utils/ImageUtils.java
@@ -1,8 +1,12 @@
 package com.heanthor.printer.utils;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.color.ICC_ColorSpace;
+import java.awt.color.ICC_Profile;
+import java.awt.image.*;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -34,7 +38,7 @@ public class ImageUtils {
         return false;
     }
 
-    public BufferedImage loadImageFromFile(String filePath) {
+    public static BufferedImage loadImageFromFile(String filePath) {
         BufferedImage img = null;
         try {
             img = ImageIO.read(new File("strawberry.jpg"));
@@ -43,5 +47,93 @@ public class ImageUtils {
         }
 
         return img;
+    }
+
+//    public static BufferedImage rgbToCMYK(BufferedImage in) {
+//        ColorConvertOp converter = new ColorConvertOp(new CMYKColorSpace(),
+//                new RenderingHints(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY));
+//
+//        return converter.filter(in, null);
+//    }
+
+    public static BufferedImage rgbToCMYK(BufferedImage in) {
+        int[][] imageInBytes = intArrayFromBufferedImage(in);
+
+        for (int[] imageInByte : imageInBytes) {
+            for (int pixel : imageInByte) {
+                int a = (pixel >> 24) & 0xff;
+                int r = (pixel & (0xff << 16)) >> 16;
+                int g = (pixel & (0xff << 8)) >> 8;
+                int b = pixel & 0xff;
+
+
+                try {
+                    float[] cmyk = rgbToCmyk((float) r / 255, (float)g / 255, (float)b / 255, (float)a / 255);
+                    System.out.println("RGB: (" + r + ", " + g + ", " + b + ") "+ "CMYK: (" + cmyk[0] + ", " + cmyk[1] + ", " + cmyk[2] + ", " + cmyk[3] + ")");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static int[][] intArrayFromBufferedImage(BufferedImage image) {
+        final int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+        int[][] result = new int[height][width];
+
+        if (hasAlphaChannel) {
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel++) {
+                int argb = 0;
+                argb += ((pixels[pixel] & 0xff) << 24); // alpha
+                argb += (pixels[pixel + 1] & 0xff); // blue
+                argb += ((pixels[pixel + 2] & 0xff) << 8); // green
+                argb += ((pixels[pixel + 3] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel++) {
+                int argb = 0;
+                argb += -16777216; // 255 alpha
+                argb += (pixels[pixel] & 0xff); // blue
+                argb += ((pixels[pixel] >> 8) & 0xff) << 8; // green
+                argb += ((pixels[pixel] >> 16) & 0xff) << 16; // red
+                result[row][col] = argb;
+                col++;
+
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static float[] rgbToCmyk(float... rgb) throws IOException {
+        String path = "C:\\Users\\reedt\\Dropbox\\IntelliJ Git\\sidewalk-printer\\resources\\icc_profiles\\USWebCoatedSWOP.icc";
+
+        if (rgb.length != 4) {
+            throw new IllegalArgumentException("Need RGBA");
+        }
+
+        ColorSpace csrgb = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+        //System.out.println("Min: " + csrgb.getMinValue(0) + ", max: " + csrgb.getMaxValue(0));
+        float[] ciexyz = csrgb.toCIEXYZ(rgb);
+        ColorSpace instance = new ICC_ColorSpace(ICC_Profile.getInstance(path));
+
+        return instance.fromCIEXYZ(ciexyz);
     }
 }
